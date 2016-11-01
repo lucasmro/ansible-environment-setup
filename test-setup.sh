a9433ddfe8e8d1bc03cbcc93f72be5e7abccf238
lucasmro <lucasmro@gmail.com>
2016-11-01 09:01:28 -0200

    Improve test-setup.sh

--- test-setup.sh
+++ test-setup.sh
@@ -3,5 +3,4 @@ cd docker && \
   docker-compose stop && \
   docker-compose rm --all -f && \
   docker build -t ansible-env-setup . && \
-  docker-compose up -d && \
-  docker-compose logs -f
+  docker-compose up
